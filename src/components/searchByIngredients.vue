5550a1276954c15d819049753abf74cb3c599289
sofiarunsten <sofiarunsten@gmail.com>
2018-05-20 13:02:28 +0200

    städat i drinksByIngredients koden och kommenterat

--- src/components/searchByIngredients.vue
+++ src/components/searchByIngredients.vue
@@ -3,9 +3,11 @@
         <form>
             <div id="selectSpirites">
                 <h3>Select ingredients</h3>
+
+                <!-- @change kör funktionen getDrinks när du väljer ingridiens, attributet disabled är kopplat till no_more_drinks -->
+                <!--för varje ingrideiens som är selected, skapas option, attributet :value är kopplat till ingredient -->
                 <select class="ingredient" @change="getDrinks" v-model="drinks.selectedIngredient" :disabled="no_more_drinks">
                   <option v-for="ingredient in drinks.drinkIngredients" :value="ingredient">{{ingredient}}</option>
-                    <!--för varje ingrideiens som är selected, skapas option och getDrinks function körs>-->
                 </select>
             </div>
         </form>
@@ -13,17 +15,21 @@
 
         <div id="yourIngredients">
 
+          <!-- rutor med ingridienserna du valt och en bild på kryss som tar bort ingridiensen om du clickar på den -->
+
           <h3 id="ingredientHeading">Your Ingredients</h3>
           <div class="ingredient-list" v-for="ingredient in drinks.ingredients">
             <p>{{ingredient}}</p>
             <img id="x" src="src/img/vittx.png" v-model="drinks.removeThisIngredient" @click="removeIngredient(ingredient)">
           </div>
           
-          
+          <!-- meddelande när det inte finns några drinkar -->
           <p>{{drinks.message}}</p>
+
         </div>
 
-        <div class="one-drink" v-for="drink in drinks.drinkFromIngredients"> <!-- h3, bild för varje drink som finns -->
+        <!-- h3 & bild för varje drink som finns i drinkFromIngredients -->
+        <div class="one-drink" v-for="drink in drinks.drinkFromIngredients">
           <router-link v-bind:to='"/drink/" + drink.idDrink'>
             <div class="grid-one-drink">
               <div class="title">
@@ -41,100 +47,89 @@
 </template>
 
 <script>
-//ingredients = det man kryssat i
-//drinkFromIngredients = objekt med alla drinkar som finns enl. ingridienser
 
 export default {
   components: {},
   data() {
     return {
-      no_more_drinks: false,
+      no_more_drinks: false, //om drinkar inte finns = true
       drinks: {
-        selectedIngredient: "", //vald ingridiens
-        ingredients: [], //array med alla selected ingredients
-        drinkFromIngredients: [], //drinks that contain selected ingredients
-        removeThisIngredient: "",
+        selectedIngredient: "", //vald ingridiens steg 1
+        ingredients: [], //array med alla valda ingredienser steg 2
+        drinkFromIngredients: [], //drinkar som innehåller valda ingridienser
+        removeThisIngredient: "", //ingridiensen du väljer att ta bort
         drinkIngredients: [], //alla ingridienser som finns i drinkarna från alcoholic drinks
-        drinkId: [], //alcoholic drinks
+        drinkId: [], //alcoholic drinks, bara namn och id
         allDrinkInfo: [], //allt om drinken
-        message: ""
+        message: "" // meddelande om det inte finns drinkar
       }
     };
   },
-  created() {
+
+  //när componenten är mounted(har tillgång till DOM), kör getAlcoholicDrinks function
+  //Om du uppdaterar sidan eller hoppar mellan sidor/componenter så finns dina valda ingridienser kvar och listan med tillhörande drinkar.
+  //när componenten är skapad så kollas det om det fanns ingridienser i sessionStorage och isf läggs dem in i ingredients och funktionen getDrinks körs
+  mounted: function() {
+    this.getAlcoholicDrinks();
+   
     if (sessionStorage.savedIngredients !== undefined) {
       this.drinks.ingredients = JSON.parse(sessionStorage.savedIngredients);
 
       this.getDrinks();
     }
-  },
 
-  //när componenten är mounted(har tillgång till DOM), kör getAlcoholicDrinks function
-  mounted: function() {
-    console.log("mounted", this);
-    this.getAlcoholicDrinks();
-  //init();
 },
 
   methods: {
-    //hämta alla drinkar i api alkoholic drinks
-    //spara drinkid i drinkId
-    //kör sen loopDrinkId
+
+    //hämta alla drinkar i api adress alkoholic drinks, spara drinkid i array, kör sen getAllDrinkInfo
+
     getAlcoholicDrinks: function() {
       this.$http
-        .get(
-          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
-        )
-        .then(function(data) {
+        .get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic")
+        .then(function(data) { 
           for (var i = 0; i < data.body.drinks.length; i++) {
             this.drinks.drinkId.push(data.body.drinks[i].idDrink);
           }
-          this.loopDrinkId();
+          this.getAllDrinkInfo();
         });
-
-      //för varje id, hitta drinkar med all info
     },
-    //loopa igenom drinkId array
-    //hämta all drinkinfo från annan api adresss
-    //spara i allDrinkInfo array
-    //kör getIngredients function
-    loopDrinkId: function() {
+
+
+    //gå in i ny api-adress och hämta mer info om alla drinkar som fanns i alcoholic drinks api-adress, och spara i array. Kör sen getIngredients
+    getAllDrinkInfo: function() {
       for (var j = 0; j < this.drinks.drinkId.length; j++) {
 
         this.$http
-          .get(
-            "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" +
-              this.drinks.drinkId[j]
-          )
-          .then(function(data) {
+          .get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + this.drinks.drinkId[j]).then(function(data) {
             for (var k = 0; k < data.body.drinks.length; k++) {
               this.drinks.allDrinkInfo.push(data.body.drinks[k]);
             }
+
             this.getIngredients();
           });
       }
     },
-    //loopa igenom allDrinkInfo array
-    //om ingridiensen inte redan finns i drinkIngredients spara i array
+
+
+    //loopa igenom allDrinkInfo array och ta ut ingridienser
+    //om ingridiensen inte redan finns i drinkIngredients array (indexOf === -1) spara den där
     getIngredients: function() {
       for (var i = 0; i < this.drinks.allDrinkInfo.length; i++) {
-        for (var j = 0; j < 15; j++){
         if (this.drinks.drinkIngredients.indexOf(this.drinks.allDrinkInfo[i].strIngredient1) === -1) {
           this.drinks.drinkIngredients.push(this.drinks.allDrinkInfo[i].strIngredient1);
         }
-        }
 
-        if (
-          this.drinks.drinkIngredients.indexOf(this.drinks.allDrinkInfo[i].strIngredient2) === -1) {
+        if (this.drinks.drinkIngredients.indexOf(this.drinks.allDrinkInfo[i].strIngredient2) === -1) {
             this.drinks.drinkIngredients.push(this.drinks.allDrinkInfo[i].strIngredient2);
         }
       }
 
+      //sortera select listan i alfabetisk ordning
       this.drinks.drinkIngredients.sort();
-      if (sessionStorage.ingredients !== undefined) { //om local storage inte är tom
-       //this.drinks.ingredients = sessionStorage.ingredients; //gör innehåll i local storage till objekt och spara i book
 
-      
+      //om session storage inte är tom, hämta innehåll (med getItem) och sätt in i ingredients array.
+      if (sessionStorage.ingredients !== undefined) {
         var retrievedData = sessionStorage.getItem("ingredients");
         this.drinks.ingredients = JSON.parse(retrievedData);
         this.getDrinks();
@@ -142,67 +137,49 @@ export default {
 }
     },
 
+
+    //hämtar drinkar utifrån dina valda ingridienser
     getDrinks: function() {
       this.drinks.drinkFromIngredients = []; 
 
 
-      //selected ingredient är string som läggs in i array ingredients
+      //när du väljer en ingridiens så läggs den in från selectedIngredient till array ingredeients
       if (this.drinks.selectedIngredient && this.drinks.ingredients.indexOf(this.drinks.selectedIngredient) === -1) {
         this.drinks.ingredients.push(this.drinks.selectedIngredient);
       }
 
-      sessionStorage.setItem("ingredients", JSON.stringify(this.drinks.ingredients));
-
       var promises = []; //array med alla anrop
 
-      //loopa igenom array med alla valda ingridienser i
-      //lägger in alla promises i array
-      //hämtar alla drinkar i anropen
-
+      //för varje vald ingridiens så pushas det specifika promiset(anropet) in i array promises
       for (var i = 0; i < this.drinks.ingredients.length; i++) {
-        //console.log("i loop");
         var ingredient = this.drinks.ingredients[i];
-
-        promises.push(
-          //lägger in alla anrop i array promises
-          this.$http.get(
-            "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +
-              ingredient
-          )
-        );
+        promises.push(this.$http.get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + ingredient));
       }
 
-        // ********************** TEST
-        sessionStorage.savedIngredients = JSON.stringify(this.drinks.ingredients);
+      sessionStorage.savedIngredients = JSON.stringify(this.drinks.ingredients);
 
+      //Promise.all väntar på alla anrop och kör then när alla anrop är färdiga
+      //responses är en array med alla svar från anrop
       Promise.all(promises).then(
-        //kör alla anrop och kör then när alla anrop är färdiga
         function(responses) {
-          //responses är en array med alla svar från anrop
+          //för varje respons, om drinklista är tom så fyll
           for (var i = 0; i < responses.length; i++) {
             var data = responses[i];
-
             if (this.drinks.drinkFromIngredients.length === 0) {
-              //om drinkFromIngredient är tom och det finns drinkar så fyll.
               for (var j = 0; j < data.body.drinks.length; j++) {
                 this.drinks.drinkFromIngredients.push(data.body.drinks[j]);
               }
 
-              //annars om drinkar enligt ny ingridiens matchar drinkar som finns i array så spara i ny array, byt sedan ut gamla array mot nya.
+              //om drinklistan inte är tom 
             } else {
-              //console.log("jämför array med ny array");
-              var drinksThisIngredient = [];
-              // console.log(i, this.drinks.drinkFromIngredients);
-              // console.log(i, data.body.drinks);
-              for (var k = 0; k < data.body.drinks.length; k++) {
-                if (
-                  // find skickar tillbaka första elementet som matchar inte true eller false.
-                  // If it finds an array element where the function returns a true value,
-                  // find() returns the value of that array element (and does not check the remaining values)
-                  // Otherwise it returns undefined
+              var temporaryDrinkArray = [];
 
-                  this.drinks.drinkFromIngredients.find(function(drink) {
-                    //vad är drink??
+              //för varje drink i data
+              //kör find (find retunerar första objektet som matchar i array) 
+              //find är funktion som kör en funktion där vi kollar om drinknamnet är samma som drinknamnet i data
+              // if find, pusha in drink i temporär array och byt sedan ut drinkByIngredients innehåll mot temporära arrayens innehåll.
+              for (var k = 0; k < data.body.drinks.length; k++) {
+                if (this.drinks.drinkFromIngredients.find(function(drink) {
                     if (drink.strDrink === data.body.drinks[k].strDrink) {
                       return true;
                     } else {
@@ -210,16 +187,16 @@ export default {
                     }
                   })
                 ) {
-                  drinksThisIngredient.push(data.body.drinks[k]);
+                  temporaryDrinkArray.push(data.body.drinks[k]); 
                 }
               }
-              this.drinks.drinkFromIngredients = drinksThisIngredient;
+              this.drinks.drinkFromIngredients = temporaryDrinkArray;
             }
 
-            // if no drinks were found stop user from selcting more ingredients!
+            // om inga drinkar fanns så stoppas användaren från att välja fler ingridienser och visa meddelande samt spara meddelande i sessionstorage
             if (this.drinks.drinkFromIngredients.length === 0) {
-              console.log("assååå")
-              this.no_more_drinks = true;
+              console.log("laddar")
+              this.no_more_drinks = true; // kopplat till disabled i <select>
               this.drinks.message = "There are no drinks with these ingredients";
               sessionStorage.setItem("message", this.drinks.message);
             } else {
@@ -230,31 +207,25 @@ export default {
       ); 
     },
 
-    removeIngredient: function(ing){
-      //ta bort ingredient i drinks.ingredients
-      //kör getDrinks
-      var valueToRemove = this.drinks.removeThisIngredient;
-      console.log("haha", ing);
+
+    //får argument från <img> i html
+    //byter ut ingredients array mot en lista där ingredientToRemove inte finns med
+    //updatera savedIngredients i sessionStorage
+    removeIngredient: function(ingredientToRemove){
 
       this.drinks.ingredients = this.drinks.ingredients.filter(function(item){
-        return item !== ing;
+        return item !== ingredientToRemove;
       });
 
       sessionStorage.savedIngredients = JSON.stringify(this.drinks.ingredients);
 
+      //nollställ selectedIngredient, selectbox disabled set to false, kör getDrinks så drinklistan uppdateras efter valda ingridienser
       this.drinks.selectedIngredient = '';
       this.no_more_drinks = false;
-      sessionStorage.removeItem("valueToRemove");
       this.getDrinks();
     }
   }
 };
-
-/*function init() {
-  methods.getDrinkIngredients();
-}
-
-window.onload = init;*/
 
 
 </script>
